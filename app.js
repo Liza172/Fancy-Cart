1faedd574c4440f4432d5c67b69b487b29715b99
Liza172 <lizarakshit@gmail.com>
2025-02-28 12:10:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const path = require('path');
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+
+const db = require("./config/mongoose-connection")
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.use(cookieParser());
+
+app.get("/", (req, res)=>
+{
+  res.send("hey");
+});
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter)
+
+
+app.listen(3000);
